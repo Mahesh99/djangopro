d451648f30f79dc9a699a8d96cbf1a831dcb5f5f
Mahesh Arya <mahesharya@prmanicus.com>
2020-05-02 20:11:22 +0530

    class 3

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+  # {
+  #   'title':'First post',
+  #   'content':'First post content',
+  #   'author':'Mahesh',
+  #   'date':'30/04/2020'
+  # } 
+class Post(models.Model):
+    title = models.CharField(max_length=200) # limited size string
+    content = models.TextField() # string , but not limit
+    create_date = models.DateTimeField(auto_now_add=True) # auto_now_add -> makes it immutable
+    modified_date = models.DateTimeField(auto_now=True) # auto_now -> makes it mutable
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.content[:25]}....'
