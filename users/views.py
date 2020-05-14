20e79fed0442d52c0f6e5149e91c159b92e6a2a4
Mahesh Arya <mahesharya@prmanicus.com>
2020-05-14 19:55:04 +0530

    profile part 1

--- users/views.py
+++ users/views.py
@@ -17,5 +17,9 @@ def register(request):
   
   return render(request,'users/register.html',{'form':form})
 
+def profile(request):
+  return render(request,'users/profile.html')
+
+
 # http codes
 # 200,404,500,400,401,301
