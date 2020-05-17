dacdf3b35cb72de8674bbbff122276853afbe4bf
Mahesh Arya <mahesharya@prmanicus.com>
2020-05-17 12:06:15 +0530

    Profile setup

--- users/views.py
+++ users/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import UserRegistrationForm
-
+from .forms import UserRegistrationForm, ProfileUpdateForm, UserUpdateForm
 
 def register(request):
   if request.method == "POST":
@@ -18,7 +17,22 @@ def register(request):
   return render(request,'users/register.html',{'form':form})
 
 def profile(request):
-  return render(request,'users/profile.html')
+  if request.method == 'POST':
+    form_p = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+    form_u = UserUpdateForm(request.POST, instance=request.user)
+    if form_u.is_valid() and form_p.is_valid():
+      form_u.save()
+      form_p.save()
+      messages.success(request,'Profile updated successfully')
+      return redirect('profile')
+  else:
+    form_p = ProfileUpdateForm(instance=request.user.profile)
+    form_u = UserUpdateForm(instance=request.user)
+  context = {
+    'form_u':form_u,
+    'form_p':form_p,
+  }
+  return render(request,'users/profile.html',context)
 
 
 # http codes
